b198a64d3ef79521ad4395b4839f499472fdf312
мансур зурабов <asl.zurabov@gmail.com>
2022-12-26 13:12:16 +0300

    handling errors

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
-
+import { Alert, Space } from "antd";
 import Header from "../Header";
 import Movies from "../Movies";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    hasError: false,
+  };
+
+  componentDidCatch() {
+    this.setState({
+      hasError: true,
+    });
+  }
+
   render() {
+    const { hasError } = this.state;
+    if (hasError) {
+      return (
+        <div className="error">
+          <Space>
+            <Alert
+              message="Error"
+              description="Sorry!!! Something wen wrong."
+              type="error"
+              showIcon
+            />
+          </Space>
+        </div>
+      );
+    }
     return (
       <div className="App">
         <Header />
--- src/Movies/Movies.js
+++ src/Movies/Movies.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Spin } from "antd";
+import { Spin, Alert, Space } from "antd";
 import GetMovies from "../service/getMovies";
 import "./Movies.css";
 import Movie from "../Movie";
@@ -9,24 +9,62 @@ class Movies extends Component {
   state = {
     movies: null,
     loading: true,
+    hasError: false,
+    intError: false,
   };
 
   componentDidMount() {
     const movies = new GetMovies();
 
-    movies.serchMovies().then((data) =>
-      this.setState(
-        {
-          movies: data.results,
-        },
-        () => this.setState({ loading: false }),
-      ),
-    );
+    movies
+      .serchMovies()
+      .then((data) =>
+        this.setState(
+          {
+            movies: data.results,
+          },
+          () => this.setState({ loading: false }),
+        ),
+      )
+      .catch((er) => {
+        if (er.message.split(" ")[0] === "NetworkError") {
+          this.setState({
+            intError: true,
+          });
+        } else {
+          this.setState({
+            hasError: true,
+          });
+        }
+      });
+  }
+
+  componentDidCatch() {
+    this.setState({
+      hasError: true,
+    });
   }
 
   render() {
-    const { movies, loading } = this.state;
-
+    const { movies, loading, hasError, intError } = this.state;
+    if (hasError || intError) {
+      return (
+        <div className="error">
+          <Space>
+            <Alert
+              message="Error"
+              description={
+                intError
+                  ? "Failed to load resource( Check your Internet)"
+                  : "Sorry!!! Something wen wrong."
+              }
+              type="error"
+              showIcon
+            />
+          </Space>
+        </div>
+      );
+    }
     let moviList = (
       <div className="spin">
         <Spin size="large" tip="Loading..." />
@@ -35,7 +73,9 @@ class Movies extends Component {
 
     if (!loading) {
       moviList = movies.map((movie) => {
-        return <Movie movie={movie} key={movie.original_title} />;
+        return (
+          <Movie movie={movie} key={`${movie.original_title}-${movie.id}`} />
+        );
       });
     }
 
